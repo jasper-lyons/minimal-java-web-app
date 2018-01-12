284fca989b7841b4eed11dfb0f91c0a8419d1e35
Jasper Lyons <jasper.lyons@gmail.com>
2018-01-12 01:03:42 +0000

    Update message

--- src/main/java/Http.java
+++ src/main/java/Http.java
@@ -4,7 +4,10 @@ import java.net.*;
 import com.sun.net.httpserver.*;
 
 public class Http {
+
   public static void main(String[] args) throws IOException {
+    // Heroku gives us a $PORT env variable that tells us where to bind our
+    // socket for listening to http traffic.
     Integer port = Integer.parseInt(System.getenv("PORT"));
     InetSocketAddress socket = new InetSocketAddress(port);
     HttpServer server = HttpServer.create(socket, 0);
@@ -17,7 +20,7 @@ public class Http {
   static class RootHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange req) throws IOException {
-      String message = "Hello";
+      String message = "<h1>Hello CS2810/CS2815/CS3811!</h1>";
 
       req.sendResponseHeaders(200, message.length());
 
